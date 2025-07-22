b40f96e4d2edc1b766dde40b8d393f3fbb931409
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-07-22 15:46:15 +0530

    Cloud Storage, PDF to Image Conversion, Key-Value Storage and AI Feedback

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from "~/components/ResumeCard";
-import { resumes } from "constants";
+import { resumes } from "../../constants";
 import { usePuterStore } from "~/lib/puter";
 import { useLocation, useNavigate } from "react-router";
 import { useEffect } from "react";
