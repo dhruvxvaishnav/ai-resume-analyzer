0432888e4ccbb7086572912dbbaa6ed120542a82
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-07-22 16:49:50 +0530

    Resume Feedback Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,24 +1,21 @@
-import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
+import Summary from "~/components/Summary";
 import ATS from "~/components/ATS";
 import Details from "~/components/Details";
-import Summary from "~/components/Summary";
-import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
-  { title: "ResuMind | Review" },
-  {
-    name: "description",
-    content: "Detailed Resume Review and Feedback",
-  },
+  { title: "Resumind | Review " },
+  { name: "description", content: "Detailed overview of your resume" },
 ];
 
-const resume = () => {
+const Resume = () => {
   const { auth, isLoading, fs, kv } = usePuterStore();
   const { id } = useParams();
   const [imageUrl, setImageUrl] = useState("");
   const [resumeUrl, setResumeUrl] = useState("");
-  const [feedback, setFeedback] = useState<Feedback | null>("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -33,19 +30,23 @@ const resume = () => {
       if (!resume) return;
 
       const data = JSON.parse(resume);
+
       const resumeBlob = await fs.read(data.resumePath);
       if (!resumeBlob) return;
+
       const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
       const resumeUrl = URL.createObjectURL(pdfBlob);
       setResumeUrl(resumeUrl);
+
       const imageBlob = await fs.read(data.imagePath);
       if (!imageBlob) return;
       const imageUrl = URL.createObjectURL(imageBlob);
       setImageUrl(imageUrl);
-      setFeedback(data.feedback);
 
-      console.log("Resume Data:", data);
+      setFeedback(data.feedback);
+      console.log({ resumeUrl, imageUrl, feedback: data.feedback });
     };
+
     loadResume();
   }, [id]);
 
@@ -53,29 +54,28 @@ const resume = () => {
     <main className="!pt-0">
       <nav className="resume-nav">
         <Link to="/" className="back-button">
-          <img src="/icons/back.svg" alt="Back" className="w-2.5 h-2.5" />
+          <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
           <span className="text-gray-800 text-sm font-semibold">
-            Back to Home
+            Back to Homepage
           </span>
         </Link>
       </nav>
       <div className="flex flex-row w-full max-lg:flex-col-reverse">
-        <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-100vh sticky top-0 items-center justify-center">
+        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
           {imageUrl && resumeUrl && (
-            <div className="animate-in fade-in gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+            <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
               <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                 <img
                   src={imageUrl}
-                  alt="Image"
                   className="w-full h-full object-contain rounded-2xl"
-                  title="Resume"
+                  title="resume"
                 />
               </a>
             </div>
           )}
         </section>
         <section className="feedback-section">
-          <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+          <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
               <Summary feedback={feedback} />
@@ -93,5 +93,4 @@ const resume = () => {
     </main>
   );
 };
-
-export default resume;
+export default Resume;
